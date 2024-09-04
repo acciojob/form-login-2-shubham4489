b8c8c762cfac3adb5e40d34bb3008ba7dcde3fc2
shubham4489 <143184073+shubham4489@users.noreply.github.com>
2024-09-04 14:35:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+function showAlert(){
+	let firstName = document.getElementById("firstName").value;
+	let lastName = document.getElementById("lastName").value;
+	let phoneNumber = document.getElementById("phoneNumber").value;
+	let emailId = document.getElementById("emailId").value;
+	 let alertMessage="First Name:"+ firstName+"Last Name"+lastName+"Phone Number:"+phoneNumber+
+		                "Email ID:"+emailId;
+	alert(alertMessage);
+}
